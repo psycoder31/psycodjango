740893670225855f1ef762a299e84dd2205cd2a5
Steve Prak <parksang21@skku.edu>
2017-07-13 15:34:07 +0900

    lesson3 완성버전

--- project/dojo/urls.py
+++ project/dojo/urls.py
@@ -1,5 +1,13 @@
+'''dojo/urls.py
+'''
+
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    
+    url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+
+    url(r'^total/(?P<numbers>[\d/]+)/$', views.total),
 ]
--- project/dojo/views.py
+++ project/dojo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+'''dojo/view.py
+'''
 
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+def mysum(request, x, y=0, z=0):
+    return HttpResponse(int(x)+int(y)+int(z))
+
+def total(request, numbers):
+    return HttpResponse(sum(map(lambda s: int(s or 0), numbers.split("/"))))
