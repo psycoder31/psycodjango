ec98b29bd63c25043d6794b7f1e045228c1555bf
Steve Prak <parksang21@skku.edu>
2017-07-17 16:55:19 +0900

    모델 벨리데이션 적용

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,4 +1,11 @@
+import re
+
+from django.forms import ValidationError
 from django.db import models
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 # Create your models here.
 
@@ -7,3 +14,5 @@ class Post(models.Model):
     content = models.TextField(verbose_name='내용')                            # 길이 제한이 없는 문자열
     created_at = models.DateTimeField(auto_now_add=True)    #데이터가 최초 저장될 때 저장
     updated_at = models.DateTimeField(auto_now=True)        #데이터가 저장 및 갱신될 때마다 저장됨
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, help_text='경도, 위도 값', blank=True, validators=[lnglat_validator])
