8018c6d496dbb7da9f920cce74bcd077fd69a3cf
Steve Prak <parksang21@skku.edu>
2017-07-17 16:48:58 +0900

    블로그의 모델을 형성하고 어드민에 등록하였다.

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='제목을 입력해 주세요 최대 90자')                # 길이 제한이 있는 문자열
+    content = models.TextField(verbose_name='내용')                            # 길이 제한이 없는 문자열
+    created_at = models.DateTimeField(auto_now_add=True)    #데이터가 최초 저장될 때 저장
+    updated_at = models.DateTimeField(auto_now=True)        #데이터가 저장 및 갱신될 때마다 저장됨
